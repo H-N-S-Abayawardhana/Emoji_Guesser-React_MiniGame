ae29a2f3756ed5745a1e85c73832f2d5cb62c363
H-N-S-Abayawardhana <hnsabayawardhana@gmail.com>
2025-04-29 21:13:53 +0530

    updated Emojicard.js file

---
+++ emoji-guesser/src/EmojiCard.js
@@ -0,0 +1,25 @@
+// EmojiCard.js - Component for displaying emoji and answer options
+import React from 'react';
+import './styles.css';
+
+const EmojiCard = ({ emoji, options, onAnswer, disabled }) => {
+  return (
+    <div className="emoji-card">
+      <div className="emoji">{emoji}</div>
+      <div className="options">
+        {options.map((option, index) => (
+          <button 
+            key={index} 
+            onClick={() => onAnswer(option)}
+            disabled={disabled}
+            className="option-btn"
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EmojiCard;
